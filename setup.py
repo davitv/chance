3be211648ca2591c70d92c8819fec73c412094d3
Davit Vardanyan <codecrime@gmail.com>
2015-10-31 13:37:47 +0200

    PyPi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'chance',
+  packages = ['chance'], 
+  version = '0.1',
+  description = 'Minimalist generator of random strings, numbers, etc.',
+  author = 'Davit Vardanyan',
+  author_email = 'davoimail@gmail.com',
+  url = 'https://github.com/davitv/chance',
+  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+  keywords = ['random', 'generator', 'chancejs'], # arbitrary keywords
+  classifiers = [],
+)

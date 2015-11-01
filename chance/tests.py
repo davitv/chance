993a0a8b517db793ac08b28b6ac890fd4d314e52
Davit Vardanyan <codecrime@gmail.com>
2015-11-01 12:38:39 +0200

    tests for url, ipv6 and country

--- chance/tests.py
+++ chance/tests.py
@@ -10,7 +10,7 @@ class TestChanceBooleanFunction(unittest.TestCase):
 
     def test_boolean_returns_bool(self):
         for x in xrange(0, 200):
-            self.assertTrue(isinstance(chance.boolean(), (bool)))
+            self.assertTrue(isinstance(chance.boolean(), bool))
 
     def test_likelihood_50_approximately_result(self):
         true_values, false_values = 0, 0
@@ -42,7 +42,6 @@ class TestChanceBooleanFunction(unittest.TestCase):
         
         self.assertTrue(res > (likelihood - infelicity))
 
-
     def test_likelihood_10_approximately_result(self):
         true_values, false_values = 0, 0
         iterations = 10000
@@ -61,7 +60,7 @@ class TestChanceBooleanFunction(unittest.TestCase):
     def test_boolean_raise_wrong_argument_exception(self):
         wrongs = (-1, -.1, 101, 101.1, 'a', 'b', '!')
         for x in wrongs:
-            self.assertRaises(chance_exceptions.WrongArgumentValue, chance.boolean, x )
+            self.assertRaises(chance_exceptions.WrongArgumentValue, chance.boolean, x)
 
 
 class TestChanceCharacterFunction(unittest.TestCase):
@@ -172,29 +171,28 @@ class TestChanceCharacterFunction(unittest.TestCase):
             ch = chance.character(case='lower', symbols=False,  numbers=True)
             self.assertTrue(chars.find(ch) >= 0)
 
-
     def test_character_raise_exception(self):
         self.assertRaises(chance_exceptions.WrongArgumentValue, chance.character, 10)
         self.assertRaises(chance_exceptions.WrongArgumentValue, chance.character, -1)
         self.assertRaises(chance_exceptions.WrongArgumentValue, chance.character, -.25)
-        self.assertRaises(chance_exceptions.DictionaryException, chance.character,  **{'language':'bbdasasdg'})
-        self.assertRaises(chance_exceptions.DictionaryException, chance.character,  **{'language':'ff'})
+        self.assertRaises(chance_exceptions.DictionaryException, chance.character,  **{'language': 'bbdasasdg'})
+        self.assertRaises(chance_exceptions.DictionaryException, chance.character,  **{'language': 'ff'})
 
 
 class TestStringFunction(unittest.TestCase):
     
     def test_string_returns_string(self):
         for x in range(100):
-            self.assertTrue(isinstance(chance.string(), (str)))
+            self.assertTrue(isinstance(chance.string(), str))
 
     def test_string_returns_string_with_correct_length(self):
         for x in xrange(1, 100):
-            self.assertTrue(len(chance.string(length=x))==x)
+            self.assertTrue(len(chance.string(length=x)) == x)
        
     def test_string_returns_string_with_correct_min_max(self):
         for x in xrange(1, 100):
             l = len(chance.string(minimum=x, maximum=x+5))
-            self.assertTrue(l >= x and l <= (x+5))
+            self.assertTrue(x <= l <= (x+5))
         
     def test_string_returns_string_from_correct_pool(self):
         pools = (
@@ -214,17 +212,18 @@ class TestStringFunction(unittest.TestCase):
         self.assertRaises(chance_exceptions.WrongArgumentValue, chance.string, 0)
         self.assertRaises(chance_exceptions.WrongArgumentValue, chance.string, 1)
         self.assertRaises(chance_exceptions.WrongArgumentValue, chance.string, .1)
-        self.assertRaises(chance_exceptions.DictionaryException, chance.string, **{'language':'dfsdf'})
+        self.assertRaises(chance_exceptions.DictionaryException, chance.string, **{'language': 'dfsdf'})
     
 
 class TestSyllableFunction(unittest.TestCase):
     def test_syllable_returns_correct_string(self):
-        syll = chance.syllable()
         for x in xrange(20):
             syll = chance.syllable(x+1)
             n = syll[0]
-            nxt = dictionaries.vowels['en'] if dictionaries.consonants['en'].find(n) != -1 else dictionaries.vowels['en']
-            prv = dictionaries.vowels['en'] if dictionaries.vowels['en'].find(n) != -1 else dictionaries.consonants['en'] 
+            nxt = dictionaries.vowels['en'] if dictionaries.consonants['en'].find(n) != -1 else \
+                dictionaries.vowels['en']
+            prv = dictionaries.vowels['en'] if dictionaries.vowels['en'].find(n) != -1 \
+                else dictionaries.consonants['en']
             for i, ch in enumerate(syll):
                 if i == 0 or i == (len(syll)-1):
                     continue
@@ -258,11 +257,11 @@ class TestSyllableFunction(unittest.TestCase):
                 nxt, prv = prv, nxt
 
     def test_syllable_raises_exception(self):
-           self.assertRaises(chance_exceptions.WrongArgumentValue, chance.syllable, 'a')
-           self.assertRaises(chance_exceptions.WrongArgumentValue, chance.syllable, dict())
-           self.assertRaises(chance_exceptions.WrongArgumentValue, chance.syllable, -1)
-           self.assertRaises(chance_exceptions.WrongArgumentValue, chance.syllable, 1.5)
-           self.assertRaises(chance_exceptions.DictionaryException, chance.syllable, **{'language':'dasdas'})
+        self.assertRaises(chance_exceptions.WrongArgumentValue, chance.syllable, 'a')
+        self.assertRaises(chance_exceptions.WrongArgumentValue, chance.syllable, dict())
+        self.assertRaises(chance_exceptions.WrongArgumentValue, chance.syllable, -1)
+        self.assertRaises(chance_exceptions.WrongArgumentValue, chance.syllable, 1.5)
+        self.assertRaises(chance_exceptions.DictionaryException, chance.syllable, **{'language': 'dasdas'})
 
 
 class TestWordFunction(unittest.TestCase):
@@ -271,14 +270,15 @@ class TestWordFunction(unittest.TestCase):
         for x in xrange(20):
             word = chance.word()
             self.assertEquals(str, type(word))
-            self.assertTrue(len(word) > 3 and len(word) < 10)
+            self.assertTrue(3 < len(word) < 10)
 
     def test_word_raises_exception(self):
         self.assertRaises(chance_exceptions.WrongArgumentValue, chance.word, 'a')
         self.assertRaises(chance_exceptions.WrongArgumentValue, chance.word, dict())
         self.assertRaises(chance_exceptions.WrongArgumentValue, chance.word, -1)
         self.assertRaises(chance_exceptions.WrongArgumentValue, chance.word, 1.5)
-        self.assertRaises(chance_exceptions.DictionaryException, chance.word, **{'language':'dasdas'})
+        self.assertRaises(chance_exceptions.DictionaryException, chance.word, **{'language': 'dasdas'})
+
 
 class TestSentenceFunction(unittest.TestCase):
     
@@ -286,7 +286,7 @@ class TestSentenceFunction(unittest.TestCase):
         for x in xrange(20):
             self.assertEquals(str, type(chance.sentence()))
             length = len(chance.sentence())
-            self.assertTrue(length >= 45 and length <= 324)
+            self.assertTrue(45 <= length <= 324)
         for x in xrange(50):
             self.assertEquals(str, type(chance.sentence(x+1)))
             
@@ -304,7 +304,7 @@ class TestSentenceFunction(unittest.TestCase):
         self.assertRaises(chance_exceptions.WrongArgumentValue, chance.sentence, 1.5)
         self.assertRaises(chance_exceptions.WrongArgumentValue, chance.sentence, 5, 12)
         self.assertRaises(chance_exceptions.WrongArgumentValue, chance.sentence, 9, 18)
-        self.assertRaises(chance_exceptions.DictionaryException, chance.sentence, **{'language':'dasdas'})
+        self.assertRaises(chance_exceptions.DictionaryException, chance.sentence, **{'language': 'dasdas'})
 
 
 class TestParagraphFunction(unittest.TestCase):
@@ -315,26 +315,24 @@ class TestParagraphFunction(unittest.TestCase):
             self.assertTrue(length >= 45)
 
 
-
 class TestAgeFunction(unittest.TestCase):
 
     def test_age_returns_correct_value(self):
         for x in xrange(20):
             self.assertEquals(int, type(chance.age()))
             a = chance.age('child')
-            self.assertTrue(a >= 1 and a <= 12 )
+            self.assertTrue(1 <= a <= 12)
             a = chance.age('teen')
-            self.assertTrue(a >= 13 and a <= 19 )
+            self.assertTrue(13 <= a <= 19)
             a = chance.age('adult')
-            self.assertTrue(a >= 18 and a <= 120 )
+            self.assertTrue(18 <= a <= 120)
             a = chance.age('senior')
-            self.assertTrue(a >= 65 and a <= 120 )
+            self.assertTrue(65 <= a <= 120)
             a = chance.age()
-            self.assertTrue(a >= 1 and a <= 120 )
+            self.assertTrue(1 <= a <= 120)
 
     def test_age_raises_exception(self):
         self.assertRaises(chance_exceptions.WrongArgumentValue, chance.age, 'i\'m')
-
 
 
 class TestDateFunction(unittest.TestCase):
@@ -356,12 +354,12 @@ class TestBirthdayFunction(unittest.TestCase):
             self.assertTrue(datetime.date(current_year + 13, 1, 1) > chance.birthday('teen'))
             self.assertTrue(datetime.date(current_year + 20, 1, 1) > chance.birthday('teen'))
 
+
 class TestFirstNameFunction(unittest.TestCase):
 
     def test_first_returns_correct_value(self):
         for x in xrange(20):
             self.assertEquals(str, type(chance.first()))
-
 
     def test_first_returns_only_gender_specified_name(self):
         from dictionaries import first_names
@@ -373,12 +371,12 @@ class TestFirstNameFunction(unittest.TestCase):
             name = chance.first(gender='female')
             self.assertTrue(name in names['f'])
 
+
 class TestLastNameFunction(unittest.TestCase):
 
     def test_first_returns_correct_value(self):
         for x in xrange(20):
             self.assertEquals(str, type(chance.last()))
-
 
     def test_first_returns_only_gender_specified_name(self):
         from dictionaries import last_names
@@ -397,11 +395,10 @@ class TestNameFunction(unittest.TestCase):
         for x in xrange(20):
             self.assertEquals(str, type(chance.name()))
 
-
     def test_name_returns_only_gender_specified_name(self):
         from dictionaries import last_names, first_names
         first_names = first_names['en']
-        last_names= last_names['en']
+        last_names = last_names['en']
         for x in xrange(20):
             name = chance.name(gender='m')
             first, last = name.split(' ')
@@ -416,7 +413,7 @@ class TestNameFunction(unittest.TestCase):
     def test_name_returns_only_language_specified_name(self):
         from dictionaries import last_names, first_names
         first_names = first_names['ru']
-        last_names= last_names['ru']
+        last_names = last_names['ru']
         for x in xrange(200):
             name = chance.name(gender='m', language='ru')
             first, last = name.split(' ')
@@ -428,11 +425,13 @@ class TestNameFunction(unittest.TestCase):
             self.assertTrue(first in first_names['f'])
             self.assertTrue(last in last_names['f'])
 
+
 class TestHashFunction(unittest.TestCase):
     
     def test_hash_returns_correct_value(self):
         for x in xrange(20):
             self.assertEquals(int, type(hash(chance.hex_hash())))
+
 
 class TestColorFunction(unittest.TestCase):
     
@@ -444,16 +443,15 @@ class TestColorFunction(unittest.TestCase):
             hex_color = chance.color(form='hex')
             rgb_color = chance.color(form='rgb')
             gray_color = chance.color(grayscale=True)
-            self.assertTrue(hex_color_pattern.match(hex_color) != None)
-            self.assertTrue(hex_color_pattern.match(gray_color) != None)
+            self.assertTrue(hex_color_pattern.match(hex_color) is not None)
+            self.assertTrue(hex_color_pattern.match(gray_color) is not None)
 
             matches = rgb_color_pattern.findall(rgb_color)
-            self.assertTrue(len(matches)==1)
-            self.assertTrue(len(matches[0])==3)
-            for x in matches[0]:
-                self.assertTrue(int(x) >= 0 and int(x) <= 255)
-            
-         
+            self.assertTrue(len(matches) == 1)
+            self.assertTrue(len(matches[0]) == 3)
+            for y in matches[0]:
+                self.assertTrue(0 <= int(y) <= 255)
+
     def test_color_raises_exception(self):
         self.assertRaises(chance_exceptions.WrongArgumentValue, chance.color, 'i\'m')
         self.assertRaises(chance_exceptions.WrongArgumentValue, chance.color, 'sitting')
@@ -468,20 +466,21 @@ class TestDomainFunction(unittest.TestCase):
         domain_pattern = re.compile('^([A-Za-z0-9]+\.[a-z\.]{2,5})$')
        
         for x in xrange(20):
-            self.assertTrue(domain_pattern.match(chance.domain()) != None)
+            self.assertTrue(domain_pattern.match(chance.domain()) is not None)
          
     def test_domain_raises_exception(self):
         self.assertRaises(chance_exceptions.WrongArgumentValue, chance.domain, 2)
         self.assertRaises(chance_exceptions.WrongArgumentValue, chance.domain, 1)
         self.assertRaises(chance_exceptions.WrongArgumentValue, chance.domain, -1)
         self.assertRaises(chance_exceptions.WrongArgumentValue, chance.domain, .1)
-   
+
+
 class TestEmailFunction(unittest.TestCase):
     def test_email_returns_correct_values(self):
         email_pattern = re.compile('^([A-Za-z0-9]+@[A-Za-z0-9]+\.[a-z\.]{2,5})$')
 
         for x in xrange(20):
-            self.assertTrue(email_pattern.match(chance.email()) != None)
+            self.assertTrue(email_pattern.match(chance.email()) is not None)
 
     def test_email_raises_exception(self):
         self.assertRaises(chance_exceptions.WrongArgumentValue, chance.email, 2)
@@ -489,11 +488,28 @@ class TestEmailFunction(unittest.TestCase):
         self.assertRaises(chance_exceptions.WrongArgumentValue, chance.email, -1)
         self.assertRaises(chance_exceptions.WrongArgumentValue, chance.email, .1)
 
+
 class TestIpFunction(unittest.TestCase):
     def test_ip_returns_correct_values(self):
         ip_pattern = re.compile('^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$')
         for x in xrange(20):
-            self.assertTrue(ip_pattern.match(chance.ip()) != None)
+            self.assertTrue(ip_pattern.match(chance.ip()) is not None)
+
+
+class TestIpv6Function(unittest.TestCase):
+    def test_ip_returns_correct_values(self):
+        ipv6_pattern = re.compile('^([0-9a-z]{4}:)+[0-9a-z]{4}$')
+        for x in xrange(20):
+            ipv6 = chance.ipv6()
+            self.assertTrue(ipv6_pattern.match(ipv6) is not None, ipv6)
+
+
+class TestTwitterFunction(unittest.TestCase):
+    def test_ip_returns_correct_values(self):
+        twitter_pattern = re.compile('^@[0-9a-z]+$')
+        for x in xrange(20):
+            twitter = chance.twitter()
+            self.assertTrue(twitter_pattern.match(twitter) is not None, twitter)
 
 
 class TestStreetFunction(unittest.TestCase):
@@ -506,6 +522,27 @@ class TestStreetFunction(unittest.TestCase):
             self.assertEquals(type(chance.street(language='ru')), unicode)
 
 
+class TestCountryFunction(unittest.TestCase):
+    def test_country_returns_string(self):
+        for x in xrange(20):
+            self.assertEquals(type(chance.country()), str)
+
+    def test_short_country(self):
+        for x in xrange(20):
+            c = chance.country(short=True)
+            self.assertTrue(1 < len(c) < 4)
+
+    def test_country_returns_correct_language(self):
+        all_states = [x['name'] for x in dictionaries.countries['en']]
+        for x in xrange(20):
+            self.assertTrue(chance.country() in all_states)
+
+    def test_country_short_returns_correct_language(self):
+        all_states = [x['code'] for x in dictionaries.countries['en']]
+        for x in xrange(20):
+            self.assertTrue(chance.country(short=True) in all_states)
+
+
 class TestStateFunction(unittest.TestCase):
     def test_state_returns_string(self):
         for x in xrange(20):
@@ -514,7 +551,7 @@ class TestStateFunction(unittest.TestCase):
     def test_short_state(self):
         for x in xrange(20):
             st = chance.state(short=True)
-            self.assertTrue(len(st) > 1 and len(st) < 4)
+            self.assertTrue(1 < len(st) < 4)
 
     def test_state_returns_correct_language(self):
         all_states = [x['name'] for x in dictionaries.states['en']]
@@ -536,7 +573,8 @@ class TestCityFuncton(unittest.TestCase):
 class TestPhoneFunction(unittest.TestCase):
     def test_phone_returns_string(self):
         for x in xrange(20):
-           self.assertEquals(str, type(chance.phone()))    
+            self.assertEquals(str, type(chance.phone()))
+
 
 class TestDictionaryFunction(unittest.TestCase):
     def test_dictionary_returns_dict(self):
@@ -568,7 +606,7 @@ class TestUnixPathFunction(unittest.TestCase):
             self.assertEquals(len(chance.path(depth=6).split('/')), 7)
 
         for x in xrange(4):
-            self.assertTrue(len(chance.path().split('/')) >= 5 and len(chance.path().split('/')) <= 7)
+            self.assertTrue(5 <= len(chance.path().split('/')) <= 7)
            
 
 class TestUnixFilePathFunction(unittest.TestCase):
@@ -582,7 +620,7 @@ class TestUnixFilePathFunction(unittest.TestCase):
         for x in xrange(20):
             fpath = chance.filepath()
             pieces = fpath.split('/')
-            self.assertTrue(len(pieces) >= 6 and len(pieces) <= 8, (len(pieces), pieces,))
+            self.assertTrue(6 <= len(pieces) <= 8, (len(pieces), pieces,))
             self.assertTrue(pieces[-1:][0].find('.') > 0)
             
     def test_unix_filepath_returns_correct_extention(self):
@@ -590,31 +628,29 @@ class TestUnixFilePathFunction(unittest.TestCase):
         for x in xrange(20):
             fpath = chance.filepath(extentions=exts)
             pieces = fpath.split('/')
-            self.assertTrue(len(pieces) >= 6 and len(pieces) <= 8, (len(pieces), pieces,))
+            self.assertTrue(6 <= len(pieces) <= 8, (len(pieces), pieces,))
             self.assertTrue(pieces[-1:][0].find('.') > 0)
             ext = pieces[-1:][0].split('.')[1]
-            self.assertTrue( any(ext == x for x in exts))
+            self.assertTrue(any(ext == x for x in exts))
         
         exts = ['txt', 'emuy']
         for x in xrange(20):
             fpath = chance.filepath(extentions=exts)
             pieces = fpath.split('/')
-            self.assertTrue(len(pieces) >= 6 and len(pieces) <= 8, (len(pieces), pieces,))
+            self.assertTrue(6 <= len(pieces) <= 8, (len(pieces), pieces,))
             self.assertTrue(pieces[-1:][0].find('.') > 0)
             ext = pieces[-1:][0].split('.')[1]
-            self.assertTrue( any(ext == x for x in exts))
+            self.assertTrue(any(ext == x for x in exts))
 
         exts = ['txt', 'emuy', 'core']
         for x in xrange(20):
             fpath = chance.filepath(extentions=exts)
             pieces = fpath.split('/')
-            self.assertTrue(len(pieces) >= 6 and len(pieces) <= 8, (len(pieces), pieces,))
+            self.assertTrue(6 <= len(pieces) <= 8, (len(pieces), pieces,))
             self.assertTrue(pieces[-1:][0].find('.') > 0)
             ext = pieces[-1:][0].split('.')[1]
-            self.assertTrue( any(ext == x for x in exts))
+            self.assertTrue(any(ext == x for x in exts))
 
-
-            
 
 if __name__ == '__main__':
     unittest.main()

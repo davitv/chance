d4c2c11a80abedf10461fbbc0f7ce0039be70c4e
Davit Vardanyan <codecrime@gmail.com>
2015-11-01 12:41:57 +0200

    new functions))

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-  name = 'chance',
-  packages = ['chance'], 
-  version = '0.1',
-  description = 'Minimalist generator of random strings, numbers, etc.',
-  author = 'Davit Vardanyan',
-  author_email = 'davoimail@gmail.com',
-  url = 'https://github.com/davitv/chance',
-  download_url = 'http://github.com/davitv/chance/tarball/0.1',
-  keywords = ['random', 'generator', 'chancejs'],
-  classifiers = [],
+    name='chance',
+    packages=['chance'],
+    version='0.102',
+    description='Minimalist generator of random strings, numbers, etc.',
+    author='Davit Vardanyan',
+    author_email='davoimail@gmail.com',
+    url='https://github.com/davitv/chance',
+    download_url='http://github.com/davitv/chance/tarball/0.102',
+    keywords=['random', 'generator', 'chancejs'],
+    classifiers=[],
 )

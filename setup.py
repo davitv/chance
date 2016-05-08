8032f6a640ce0dc2f5e6d79e53ef1e032a8ee649
Davit Vardanyan <davoimail@gmail.com>
2016-05-08 14:04:36 +0300

    gitignore for pypirc and new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name='chance',
     packages=['chance'],
-    version='0.102',
+    version='0.103',
     description='Minimalist generator of random strings, numbers, etc.',
     author='Davit Vardanyan',
     author_email='davoimail@gmail.com',
